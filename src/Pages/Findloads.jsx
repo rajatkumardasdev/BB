276e5fdd21657237371994867e74c1406ea85b48
Rajat Kumar Das <rajatkumardas.dev@gmail.com>
2026-05-22 11:07:50 +0000

    gg

--- src/Pages/Findloads.jsx
+++ src/Pages/Findloads.jsx
@@ -400,7 +400,7 @@ const FindLoads = () => {
                                 {/* BOOK */}
                                 <button
                                     className="btn w-100 mt-4 rounded-pill fw-bold "
-                                    style={{  background: "#e63947a0",
+                                    style={{  background: "#575cf2",
                                 color: "#fff", }}
                                     onClick={() => handleBookOrder(item)}
                                 >
